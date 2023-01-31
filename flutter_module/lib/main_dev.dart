05377a7b010a5562817784c5e0fbafdf87f23e24
Lorenzo Miuccio <lorenzo.miuccio@sysdata.it>
2023-01-31 15:22:32 +0100

    add flavors

--- flutter_module/lib/main_dev.dart
+++ flutter_module/lib/main_dev.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_module/app.dart';
 import 'package:flutter_module/config/env.dart';
 
+export 'package:flutter_module/main_prod.dart';
 
 @pragma('vm:entry-point')
 void main() => runApp(MyApp(Env.dev()));
