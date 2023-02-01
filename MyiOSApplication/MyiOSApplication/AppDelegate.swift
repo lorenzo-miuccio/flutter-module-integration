4980a3b3e68bf49576f775038ab9ec15c142e565
Lorenzo Miuccio <lorenzo.miuccio@sysdata.it>
2023-02-01 13:08:56 +0100

    fix entrypoint ios

--- MyiOSApplication/MyiOSApplication/AppDelegate.swift
+++ MyiOSApplication/MyiOSApplication/AppDelegate.swift
@@ -14,8 +14,7 @@ class AppDelegate: FlutterAppDelegate {
     lazy var flutterEngine = FlutterEngine(name: "my flutter engine")
 
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Runs the default Dart entrypoint with a default Flutter route.
-        flutterEngine.run(withEntrypoint: "main", libraryURI: "main_dev.dart");
+        flutterEngine.run(withEntrypoint: "main", libraryURI: "package:flutter_module/main_dev.dart");
         // Connects plugins with iOS platform code to this app.
         GeneratedPluginRegistrant.register(with: self.flutterEngine);
         return super.application(application, didFinishLaunchingWithOptions: launchOptions);
