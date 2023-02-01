d96f04d3d365b8cbbf92bc428ec8bc6a37a195fb
Lorenzo Miuccio <lorenzo.miuccio@sysdata.it>
2023-02-01 12:35:56 +0100

    add entry point in ios

--- MyiOSApplication/MyiOSApplication/AppDelegate.swift
+++ MyiOSApplication/MyiOSApplication/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: FlutterAppDelegate {
 
     override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Runs the default Dart entrypoint with a default Flutter route.
-        flutterEngine.run();
+        flutterEngine.run(withEntrypoint: "main", libraryURI: "main_dev.dart");
         // Connects plugins with iOS platform code to this app.
         GeneratedPluginRegistrant.register(with: self.flutterEngine);
         return super.application(application, didFinishLaunchingWithOptions: launchOptions);
